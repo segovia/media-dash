e8e3b494f5668ff7907521a3d935fe4502b413d1
Gustavo Segovia <gustavo.segovia@gmail.com>
2017-09-18 23:30:25 +0200

    Recursive file listing

--- torrent-dash-back/media_files.js
+++ torrent-dash-back/media_files.js
@@ -1,18 +1,62 @@
-const {promisify} = require("util");
+const { promisify } = require("util");
 const glob = promisify(require("glob"));
 
 
 module.exports = class MediaFiles {
-    
+
     constructor(mediaDir) {
         this.mediaDir = mediaDir;
         console.log("Media directory: " + this.mediaDir);
     }
     async getFileListing() {
         console.log("MediaFiles - INFO: Scanning file listing");
-        const files = await glob(this.mediaDir + "/**/*", {nodir: true});
+        const files = await glob(this.mediaDir + "/**/*", { nodir: true });
         console.log("MediaFiles - INFO: Files scanned");
-        // filter out unneeded files
-        return files.filter(s => !s.endsWith(".srt")).map(s => s.substring(this.mediaDir.length + 1));
+        const filteredFiles = files.filter(s => !s.endsWith(".srt")).map(s => s.substring(this.mediaDir.length + 1));
+        return this._toTree(filteredFiles);
+    }
+
+    _toTree(fileList) {
+        const root = new Folder("");
+        fileList.forEach(file => this._toTreeRecursive(file, file.charAt(0) === "/" ? 1 : 0, root));
+        return root;
+    }
+
+    _toTreeRecursive(filePath, curIndex, parent) {
+        if (curIndex >= filePath.length) return;
+        const idx = filePath.indexOf("/", curIndex);
+        const token = filePath.substring(curIndex, idx === -1 ? filePath.length : idx);
+        if (idx == -1) {
+            parent.addFile(token);
+            return;
+        }
+        const folder = parent.findOrCreateChild(token);
+        this._toTreeRecursive(filePath, idx + 1, folder);
     }
 };
+
+class File {
+    constructor(name) {
+        this.name = name;
+        this.type = "FILE";
+    }
+}
+
+class Folder {
+    constructor(name) {
+        this.name = name;
+        this.type = "FOLDER";
+        this.children = [];
+    }
+    addFile(name) {
+        this.children.push(new File(name));
+    }
+    findOrCreateChild(name) {
+        let child = this.children.find(f => f.name === name && f.type === this.type);
+        if (!child) {
+            child = new Folder(name);
+            this.children.push(child);
+        }
+        return child;
+    }
+}
