18ba8f9e3e106f32b25f716d0df594207391049c
Gustavo Segovia <gustavo.segovia@gmail.com>
2017-09-22 01:45:53 +0200

    Throttle requests to tmdb

--- media-dash-back/media.js
+++ media-dash-back/media.js
@@ -17,22 +17,37 @@ module.exports = class Media {
         if (hasChanged) this.ct.cache.persistCache(this.cacheKey, this.mediaListing);
     }
 
+    getMediaListing(){
+        return this.mediaListing;
+    }
+
     async _updateInfoIfNeeded() {
-        const movies = this.mediaListing.children[0];
+        const sleepTime = 10000;
         let updated = false;
-        const promises = movies.children.map(async (movieFolder) => {
-            if (movieFolder.imdbId) {
-                console.log(`${movieFolder.name} -> skipped!`);
-                return;
-            }
+        while (await this._updateBatch(15)) {
+            console.log(`\nWaiting ${sleepTime/1000}s to start next batch...\n`);
+            await this._sleep(sleepTime);
+            updated = true;
+        }
+        return updated;
+    }
+
+    async _updateBatch(batchSize) {
+        const movies = this.mediaListing.children[0];
+        const moviesWithoutId = movies.children.filter(movieFolder => movieFolder.imdbId == undefined || movieFolder == null);
+        if (moviesWithoutId.length == 0) return false;
+        const promises = moviesWithoutId.slice(0, batchSize).map(async (movieFolder) => {
             const imdbId = await this.ct.extMediaInfo.getMovieImdbId(movieFolder.name);
             console.log(`${movieFolder.name} -> ${imdbId}`);
-            if (imdbId) {
+            if (imdbId != null) {
                 movieFolder.imdbId = imdbId;
-                updated = true;
             }
         });
         await Promise.all(promises);
-        return updated;
+        return moviesWithoutId.length > batchSize;
+    }
+
+    _sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
     }
 };
